83d700b11039c85ec1a260e39bee19728e49848c
Bhanu Pratap <ppratapbhanu0@gmail.com>
2026-07-15 15:38:36 +0100

    New Updation

--- Array/Rrevision Array.py
+++ Array/Rrevision Array.py
@@ -1,4 +1,14 @@
-import numpy as np
+from numpy import *
+a = arange(1,10)
+print(a)
+b = a[1:7:2]
+print(b)
+c = a[::]
+print(c)
+# d =a[::-1]
+# print(d)
+e = a[-1:4:-1]
+print(e)
 
-df  = 
-print(df)
+
+
